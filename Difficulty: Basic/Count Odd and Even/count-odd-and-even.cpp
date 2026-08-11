da7e9319ee82e7f240d236da125013c62d060083
Gona Lalith <gonalalith2005@gmail.com>
2026-08-11 21:27:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    pair<int, int> countOddEven(vector<int> &arr) {
+        // code here
+        int odd = 0;
+        int even = 0;
+        for(int i=0;i<arr.size();i++) {
+            if(arr[i]%2==0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return make_pair(odd, even);
+    }
+};
