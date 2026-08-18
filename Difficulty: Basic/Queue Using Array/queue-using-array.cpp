ff34d8a06c92b7c79c3c89c5fb107f2f391bcb00
Gona Lalith <gonalalith2005@gmail.com>
2026-08-18 15:08:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue Using Array/queue-using-array.cpp
@@ -0,0 +1,48 @@
+class myQueue {
+    int *arr;
+    int size;
+    int front;
+    int rear;
+    int count;
+  public:
+    myQueue(int n) {
+        size = n;
+        arr = new int[n];
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+    bool isEmpty() {
+        return count == 0;
+    }
+    bool isFull() {
+        return count == size;
+    }
+    void enqueue(int x) {
+        if (isFull()) {
+            return;
+        }
+        rear = (rear + 1) % size;
+        arr[rear] = x;
+        count++;
+    }
+    void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+        front = (front + 1) % size;
+        count--;
+    }
+    int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[front];
+    }
+    int getRear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[rear];
+    }
+};
