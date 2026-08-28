9d958222026ee9ed324f147ff3c770f2465b0efc
Gona Lalith <gonalalith2005@gmail.com>
2026-08-28 23:04:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array with All Palindromes/array-with-all-palindromes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool isPalinArray(vector<int> &arr) {
+        for (int num : arr) {
+            int original = num;
+            int reversed = 0;
+
+            while (num > 0) {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+
+            if (original != reversed)
+                return false;
+        }
+
+        return true;
+    }
+};
