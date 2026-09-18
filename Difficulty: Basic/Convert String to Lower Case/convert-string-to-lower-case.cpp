855c44b6c7452ff717e18de63212e0dd10cf6d99
Gona Lalith <gonalalith2005@gmail.com>
2026-09-18 20:56:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to Lower Case/convert-string-to-lower-case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLower(string s) {
+        for (int i = 0; i < s.size(); i++) {
+            s[i] = tolower(s[i]);
+        }
+
+        return s;
+    }
+};
